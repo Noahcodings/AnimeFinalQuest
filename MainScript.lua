9d1188a9ac8d9b34e8917947db9a3463ff619045
Noahcodings <noahfabon15@gmail.com>
2025-12-23 23:40:50 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,4 @@
-if game.PlaceId = 
+if game.PlaceId = 100744519298647
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Testing script | Anime Final Quest [BETA]", IntroText = "tangina mo aron", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
