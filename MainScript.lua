f999bc3173fc58f98627772dc62920b542864073
Noahcodings <noahfabon15@gmail.com>
2025-12-24 00:03:26 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,21 +1,6 @@
 if game.PlaceId == 100744519298647 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Testing script | Anime Final Quest [BETA]", IntroText = "tangina mo aron", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
---Funtions
-	_G.AutoRoll = true
-	_G.autoWeapon = (NormalSpin)
---Tabs
---Togles
---DropDowns
-	AutoWeapon:AddDropdown({
-	Name = "SelectOdds",
-	Default = "NormalSpin",
-	Options = {"NormalSpin", "LuckySpin"},
-	Callback = function(Value)
-		print(Value)
-	end    
-})
-
 
 end
 OrionLib:Init()
