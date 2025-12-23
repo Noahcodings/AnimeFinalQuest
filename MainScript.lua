a9ef4348b724ff5e2b4bb535f7df148448297a3e
Noahcodings <noahfabon15@gmail.com>
2025-12-24 00:10:39 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,47 @@
-if game.PlaceId == 100744519298647 then
+if game.PlaceId = 100744519298647 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Testing script | Anime Final Quest [BETA]", IntroText = "tangina mo aron", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-end
+--Funtions
+function AutoWeapon()
+	game:GetService("ReplicatedStorage").BridgeNet2.dataRemoteEvent:FireServer({"NormalSpin","LuckySpin"})
+	wait(.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
+--Values
+_G.AutoWeapon = true
+_G.SelectOdds = "LuckySpin, NormalSpin"
+
+--Tabs
+local RollTab = Window:MakeTab({
+	Name = "Weapon",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local RollTab = Window:MakeTab({
+	Name = "Gears",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--Toggles
+AutoWeapon:AddToggle({
+	Name = "Auto Roll",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+
+--DrowDowns
+AutoWeapon:AddDropdown({
+	Name = "SelectOdds",
+	Default = "LuckySpin",
+	Options = {"LuckySpin", "NormalSpin"},
+	Callback = function(Value)
+		_G.SelectOdds = Value
+		print(_G.SelectOdds)
+	end    
+})
+	end
 OrionLib:Init()
