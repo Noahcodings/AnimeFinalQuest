3ff2fbc5b34ad7485b0114ba2868a81c65f7be43
Noahcodings <noahfabon15@gmail.com>
2025-12-24 00:22:10 +0800

    Fix assignment operator in PlaceId check

--- MainScript.lua
+++ MainScript.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 100744519298647 then
+if game.PlaceId = 100744519298647 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Testing script | Anime Final Quest [BETA]", IntroText = "tangina mo aron", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
